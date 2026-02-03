d6e16546940f66b97e9762d4359f2d0ed6886b67
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-02-04 02:23:10 +0900

    WeAct_RP2350B

--- configs/WeAct_RP2350B/BoardConfig.h
+++ configs/WeAct_RP2350B/BoardConfig.h
@@ -1,106 +1,37 @@
 #ifndef BOARD_CONFIG_H
 #define BOARD_CONFIG_H
 
-#define BOARD_NAME "WeAct RP2350B Full Custom"
+#include "enums.pb.h"
 
-// ============================================================
-// Main pin mapping (GP2040 | Xinput | Switch | PS3/4/5 | Dinput)
-// ============================================================
-//                                                  // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
-#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_UP     // UP     | UP     | UP      | UP       | UP     | UP     |
-#define GPIO_PIN_03 GpioAction::BUTTON_PRESS_DOWN   // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
-#define GPIO_PIN_04 GpioAction::BUTTON_PRESS_RIGHT  // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
-#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_LEFT   // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
-#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_B1     // B1     | A      | B       | Cross    | 2      | K1     |
-#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
-#define GPIO_PIN_08 GpioAction::BUTTON_PRESS_R2     // R2     | RT     | ZR      | R2       | 8      | K3     |
-#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_L2     // L2     | LT     | ZL      | L2       | 7      | K4     |
-#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
-#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
-#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
-#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
-#define GPIO_PIN_16 GpioAction::BUTTON_PRESS_S1     // S1     | Back   | Minus   | Select   | 9      | Coin   |
-#define GPIO_PIN_17 GpioAction::BUTTON_PRESS_S2     // S2     | Start  | Plus    | Start    | 10     | Start  |
-#define GPIO_PIN_18 GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
-#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_R3     // R3     | RS     | RS      | R3       | 12     | RS     |
-#define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A1     // A1     | Guide  | Home    | PS       | 13     | ~      |
-#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_A2     // A2     | ~      | Capture | ~        | 14     | ~      |
+#define BOARD_NAME "WeAct RP2350B Full"
 
-// ============================================================
-// アドオン用ピンの初期定義 (Web Configuratorで変更可能)
-// ============================================================
-#define GPIO_PIN_00 GpioAction::ASSIGNED_TO_ADDON    // I2C0 SDA
-#define GPIO_PIN_01 GpioAction::ASSIGNED_TO_ADDON    // I2C0 SCL
-#define GPIO_PIN_14 GpioAction::BUTTON_PRESS_TURBO   // Turbo Button
-#define GPIO_PIN_15 GpioAction::ASSIGNED_TO_ADDON    // Turbo LED
-#define GPIO_PIN_28 GpioAction::ASSIGNED_TO_ADDON    // RGB NeoPixel
+// メインボタン配置 (最新の構造体形式)
+#define GPIO_PIN_02 { .action = BUTTON_PRESS_UP,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_03 { .action = BUTTON_PRESS_DOWN,  .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_04 { .action = BUTTON_PRESS_RIGHT, .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_05 { .action = BUTTON_PRESS_LEFT,  .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_06 { .action = BUTTON_PRESS_B1,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_07 { .action = BUTTON_PRESS_B2,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_08 { .action = BUTTON_PRESS_R2,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_09 { .action = BUTTON_PRESS_L2,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_10 { .action = BUTTON_PRESS_B3,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_11 { .action = BUTTON_PRESS_B4,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_12 { .action = BUTTON_PRESS_R1,    .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_13 { .action = BUTTON_PRESS_L1,    .direction = GPIO_DIRECTION_INPUT }
 
-// ============================================================
-// WeAct RP2350B 拡張ピン (GP30 - GP47) の有効化
-// ============================================================
-// デフォルトで「機能なし」として全ピンをWeb Configから選択可能にする
-#define GPIO_PIN_30 GpioAction::NONE
-#define GPIO_PIN_31 GpioAction::NONE
-#define GPIO_PIN_32 GpioAction::NONE
-#define GPIO_PIN_33 GpioAction::NONE
-#define GPIO_PIN_34 GpioAction::NONE
-#define GPIO_PIN_35 GpioAction::NONE
-#define GPIO_PIN_36 GpioAction::NONE
-#define GPIO_PIN_37 GpioAction::NONE
-#define GPIO_PIN_38 GpioAction::NONE
-#define GPIO_PIN_39 GpioAction::NONE
+// 拡張ピン (30-47) をシステムに認識させるための最小定義
+// これを記述することで Web Configurator のドロップダウンに番号が出現します
+#define GPIO_PIN_30 { .action = BUTTON_PRESS_NONE, .direction = GPIO_DIRECTION_INPUT }
+#define GPIO_PIN_40 { .action = BUTTON_PRESS_NONE, .direction = GPIO_DIRECTION_INPUT }
 
-// ============================================================
-// アナログ入力設定 (WeAct RP2350B: GP40-47)
-// ============================================================
-#define ANALOG_INPUT_ENABLED 1
-#define PIN_ANALOG_X 40 // Left Stick X
-#define PIN_ANALOG_Y 41 // Left Stick Y
-// 拡張ADCピンをアドオン等で利用可能に設定
-#define ADC_MAX_CHANNELS 8 
+// アナログ入力有効化
+#define HAS_ANALOG_INPUT 1
 
-// Keyboard Mapping Configuration
-//                                            // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
-#define KEY_DPAD_UP     HID_KEY_ARROW_UP      // UP     | UP     | UP      | UP       | UP     | UP     |
-#define KEY_DPAD_DOWN   HID_KEY_ARROW_DOWN    // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
-#define KEY_DPAD_RIGHT  HID_KEY_ARROW_RIGHT   // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
-#define KEY_DPAD_LEFT   HID_KEY_ARROW_LEFT    // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
-#define KEY_BUTTON_B1   HID_KEY_SHIFT_LEFT    // B1     | A      | B       | Cross    | 2      | K1     |
-#define KEY_BUTTON_B2   HID_KEY_Z             // B2     | B      | A       | Circle   | 3      | K2     |
-#define KEY_BUTTON_R2   HID_KEY_X             // R2     | RT     | ZR      | R2       | 8      | K3     |
-#define KEY_BUTTON_L2   HID_KEY_V             // L2     | LT     | ZL      | L2       | 7      | K4     |
-#define KEY_BUTTON_B3   HID_KEY_CONTROL_LEFT  // B3     | X      | Y       | Square   | 1      | P1     |
-#define KEY_BUTTON_B4   HID_KEY_ALT_LEFT      // B4     | Y      | X       | Triangle | 4      | P2     |
-#define KEY_BUTTON_R1   HID_KEY_SPACE         // R1     | RB     | R       | R1       | 6      | P3     |
-#define KEY_BUTTON_L1   HID_KEY_C             // L1     | LB     | L       | L1       | 5      | P4     |
-#define KEY_BUTTON_S1   HID_KEY_5             // S1     | Back   | Minus   | Select   | 9      | Coin   |
-#define KEY_BUTTON_S2   HID_KEY_1             // S2     | Start  | Plus    | Start    | 10     | Start  |
-#define KEY_BUTTON_L3   HID_KEY_EQUAL         // L3     | LS     | LS      | L3       | 11     | LS     |
-#define KEY_BUTTON_R3   HID_KEY_MINUS         // R3     | RS     | RS      | R3       | 12     | RS     |
-#define KEY_BUTTON_A1   HID_KEY_9             // A1     | Guide  | Home    | PS       | 13     | ~      |
-#define KEY_BUTTON_A2   HID_KEY_F2            // A2     | ~      | Capture | ~        | 14     | ~      |
-#define KEY_BUTTON_FN   -1                    // Hotkey Function                                        |
-
-
-// ============================================================
-// アドオン全機能の有効化フラグ
-// ============================================================
-#define TURBO_ENABLED 1
-#define BOARD_LEDS_ENABLED 1
-#define HAS_I2C_DISPLAY 1
-#define I2C0_ENABLED 1
-#define I2C0_PIN_SDA 0
-#define I2C0_PIN_SCL 1
-
-// RGB LED詳細
+// I2C / LED 設定 (WeActのデフォルトピンに合わせて適宜調整してください)
+#define HAS_I2C 1
+#define I2C0_SDA 0
+#define I2C0_SCL 1
 #define BOARD_LEDS_PIN 28
-#define LED_BRIGHTNESS_MAXIMUM 100
-#define LED_BRIGHTNESS_STEPS 5
-#define LED_FORMAT LED_FORMAT_GRB
-
-// レイアウト初期値
-#define BUTTON_LAYOUT BUTTON_LAYOUT_STICKLESS
-#define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_STICKLESSB
 
 #endif
 
