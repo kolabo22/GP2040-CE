3e2bf88255990ba3cf3e823f70517eebbfdbc378
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-02-04 03:00:02 +0900

    WeAct_RP2350B

--- configs/WeAct_RP2350B/BoardConfig.h
+++ configs/WeAct_RP2350B/BoardConfig.h
@@ -1,3 +1,4 @@
+
 #ifndef BOARD_CONFIG_H
 #define BOARD_CONFIG_H
 
@@ -5,34 +6,27 @@
 
 #define BOARD_NAME "WeAct RP2350B Full"
 
-// メインボタン配置 (最新の構造体形式)
+// メインボタン配置 (必要最低限に絞り、重複を避けます)
 #define GPIO_PIN_02 { .action = BUTTON_PRESS_UP,    .direction = GPIO_DIRECTION_INPUT }
 #define GPIO_PIN_03 { .action = BUTTON_PRESS_DOWN,  .direction = GPIO_DIRECTION_INPUT }
 #define GPIO_PIN_04 { .action = BUTTON_PRESS_RIGHT, .direction = GPIO_DIRECTION_INPUT }
 #define GPIO_PIN_05 { .action = BUTTON_PRESS_LEFT,  .direction = GPIO_DIRECTION_INPUT }
 #define GPIO_PIN_06 { .action = BUTTON_PRESS_B1,    .direction = GPIO_DIRECTION_INPUT }
 #define GPIO_PIN_07 { .action = BUTTON_PRESS_B2,    .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_08 { .action = BUTTON_PRESS_R2,    .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_09 { .action = BUTTON_PRESS_L2,    .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_10 { .action = BUTTON_PRESS_B3,    .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_11 { .action = BUTTON_PRESS_B4,    .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_12 { .action = BUTTON_PRESS_R1,    .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_13 { .action = BUTTON_PRESS_L1,    .direction = GPIO_DIRECTION_INPUT }
 
-// 拡張ピン (30-47) をシステムに認識させるための最小定義
-// これを記述することで Web Configurator のドロップダウンに番号が出現します
-#define GPIO_PIN_30 { .action = BUTTON_PRESS_NONE, .direction = GPIO_DIRECTION_INPUT }
-#define GPIO_PIN_40 { .action = BUTTON_PRESS_NONE, .direction = GPIO_DIRECTION_INPUT }
+// 設定モード起動用ピン (重要)
+#define FORCED_SETUP_MODE_PIN 30 
 
 // アナログ入力有効化
 #define HAS_ANALOG_INPUT 1
 
-// I2C / LED 設定 (WeActのデフォルトピンに合わせて適宜調整してください)
+// I2C設定 (ディスプレイ用)
 #define HAS_I2C 1
 #define I2C0_SDA 0
 #define I2C0_SCL 1
+
+// LED設定
 #define BOARD_LEDS_PIN 28
 
 #endif
-
 
